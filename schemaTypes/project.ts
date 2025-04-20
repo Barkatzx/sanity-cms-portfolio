70494fd2387a5917015938f7517968727cbe4601
Barkat Ullah <barkatullah.zx@gmail.com>
2025-04-20 19:16:11 +0600

    Multiple Photo Upload Defined

--- schemaTypes/project.ts
+++ schemaTypes/project.ts
@@ -2,22 +2,37 @@ import {defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'project',
-  title: 'Project',
+  title: 'Projects',
   type: 'document',
   fields: [
     defineField({
-      name: 'photo',
-      title: 'Photo',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
+      name: 'photos',
+      title: 'Photos',
+      type: 'array',
+      of: [
+        {
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
     }),
     defineField({
       name: 'title',
       title: 'Title',
       type: 'string',
     }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+
     defineField({
       name: 'description',
       title: 'Description',
